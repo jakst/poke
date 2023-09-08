b7274d5ced29013d000dc32258a3182cda38ca9c
Jakob Norlin <jakob.stahl91@gmail.com>
2023-09-08 20:54:32 +0200

    Use React's PropsWithChildren

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { PropsWithChildren } from 'react'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,11 +10,7 @@ export const metadata: Metadata = {
   description: 'The complete Pokemon collection, all in one place.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
