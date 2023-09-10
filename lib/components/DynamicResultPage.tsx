8fc9e0eaf55dc2f9fc57e2cd31fc082f2a1d7e56
Jakob Norlin <jakob.stahl91@gmail.com>
2023-09-10 15:57:59 +0200

    Automatically load more content while scrolling, using IntersectionObserver

--- lib/components/DynamicResultPage.tsx
+++ lib/components/DynamicResultPage.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import { useState } from "react"
+import { css } from "@/pokestyle/css"
+import { flex } from "@/pokestyle/patterns"
+import { useEffect, useRef, useState } from "react"
 import useSWR from "swr"
 import { api } from "../api"
 import { ListItem } from "./ListItem"
@@ -10,14 +12,36 @@ interface Props {
 }
 
 export function DynamicResultPage({ url }: Props) {
-	const { data } = useSWR(url, api.getPokemonList)
+	const { data, isLoading } = useSWR(url, api.getPokemonList)
 
 	const [showNextPage, setShowNextPage] = useState(false)
 
+	const loadMoreTriggerRef = useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		if (!loadMoreTriggerRef.current || !data) return
+
+		const observer = new IntersectionObserver((entries) => {
+			if (entries[0].isIntersecting) {
+				setShowNextPage(true)
+			}
+		})
+
+		observer.observe(loadMoreTriggerRef.current)
+
+		return () => {
+			observer.disconnect()
+		}
+	}, [data])
+
+	if (isLoading) return <LoadingIndicator />
 	if (!data) return null
 
 	return (
 		<>
+			{/* When this element is scrolled into the viewport, we start loading the next page */}
+			<div ref={loadMoreTriggerRef} className={css({ visibility: "hidden" })} />
+
 			{data.results.map((pokemon) => (
 				<ListItem
 					key={pokemon.name}
@@ -26,13 +50,23 @@ export function DynamicResultPage({ url }: Props) {
 				/>
 			))}
 
-			{showNextPage ? (
-				<DynamicResultPage url={data.next!} />
-			) : (
-				data.next && (
-					<button onClick={() => setShowNextPage(true)}>Load more</button>
-				)
-			)}
+			{showNextPage && data.next && <DynamicResultPage url={data.next} />}
 		</>
+	)
+}
+
+function LoadingIndicator() {
+	return (
+		<div
+			className={flex({
+				w: "12rem",
+				align: "center",
+				justify: "center",
+				flex: 1,
+				padding: "2rem 1rem 4rem",
+			})}
+		>
+			Loading more...
+		</div>
 	)
 }
